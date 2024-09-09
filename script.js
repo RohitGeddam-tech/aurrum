1b236c5fedcd48f7ffad84210ae11d29f2e0551d
cod3nator <asishbosek@gmail.com>
2024-09-09 21:37:47 +0530

    Added new section

--- script.js
+++ script.js
@@ -2,20 +2,42 @@ const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
+
   slidesPerView: 2,
   spaceBetween: 1,
 
   breakpoints: {
-    // when window width is >= 320px
     640: {
       slidesPerView: 1,
-      spaceBetween: 0
-    }
+      spaceBetween: 0,
+    },
+
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
   },
 
-  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-prev",
+    prevEl: ".swiper-button-next",
+  },
+});
+
+const swiperBanner = new Swiper(".banner-swiper", {
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiperGallery = new Swiper('.gallery-swiper', {
+  loop: true,
+
+  slidesPerView: 3,
+  navigation: {
+    nextEl: '.gallery-swiper-button-next',
+    prevEl: '.gallery-swiper-button-prev',
   },
 });
