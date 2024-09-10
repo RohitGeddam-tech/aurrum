fd19f74cec15eff2c79933bac113d111acf2c07a
cod3nator <asishbosek@gmail.com>
2024-09-10 16:53:32 +0530

    Changed swiper to slick slider

--- script.js
+++ script.js
@@ -1,43 +1,82 @@
+
+
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
   direction: "horizontal",
   loop: true,
-
   slidesPerView: 2,
   spaceBetween: 1,
-
   breakpoints: {
     640: {
       slidesPerView: 1,
       spaceBetween: 0,
     },
-
     1024: {
       slidesPerView: 2,
       spaceBetween: 30,
     },
   },
-
-  navigation: {
-    nextEl: ".swiper-button-prev",
-    prevEl: ".swiper-button-next",
-  },
-});
-
-const swiperBanner = new Swiper(".banner-swiper", {
-  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 });
 
-const swiperGallery = new Swiper('.gallery-swiper', {
-  loop: true,
+// const swiperBanner = new Swiper(".banner-swiper", {
+//   loop: true,
+//   direction: "horizontal",
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+// const ExpandingCardBanner = new Swiper(".expanding-cards", {
+//   loop: false,
+//   slidesPerView: 5,
+//   spaceBetween: 0, // No space between slides
+//   navigation: {
+//     nextEl: ".expand-card-button-next",
+//     prevEl: ".expand-card-button-prev",
+//   },
+// });
+// const swiperGallery = new Swiper('.gallery-swiper', {
+//   loop: true,
+//   centeredSlides: true,
+//   watchOverflow: true,
+//   updateOnWindowResize: true,
+//   loopSlides: 7,
+//   slidesPerView: 3,
+//   spaceBetween: 36,
+//   navigation: {
+//     nextEl: '.gallery-swiper-button-next',
+//     prevEl: '.gallery-swiper-button-prev',
+//   },
+// });
 
-  slidesPerView: 3,
-  navigation: {
-    nextEl: '.gallery-swiper-button-next',
-    prevEl: '.gallery-swiper-button-prev',
-  },
-});
+// $('.expanding-cards').slick({
+//   centerMode: true,
+//   centerPadding: '10px',
+//   slidesToShow: 3,
+  
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '40px',
+//         slidesToShow: 3
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '40px',
+//         slidesToShow: 1
+//       }
+//     }
+//   ]
+// });
+
+
